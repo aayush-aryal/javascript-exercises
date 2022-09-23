ce8468df9defeb052c1461378b22492047af3cfd
Aayush Aryal <rinnegan2024@gmail.com>
2022-09-23 21:13:47 +0545

    added solutions till 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(random_string,input_number) {
+    if (input_number>=0) {
+        let repeated=''
+        let number=input_number
+        strin=random_string.toString();
+        for (let j=0; j<number ; j++) {
+            repeated+=strin
+        }
+    return repeated;    
+    }else {
+        return 'ERROR'
+    }   
 
-};
+
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(user_string) {
+    let conv= user_string.toString();
+    let reversed='';
+    conv_array= conv.split('')
+    for(let i=conv_array.length-1; i>-1; i--) {
+        reversed+=conv_array[i];
+
+    }
+
+    return reversed;
 
 };
 
