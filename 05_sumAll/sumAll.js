67d67d674f6fb267faec377e63afc69538c85fb8
Aayush Aryal <rinnegan2024@gmail.com>
2022-09-24 11:05:24 +0545

    added solutions to exercise 5,6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(number1,number2) {
+    if (Number.isInteger(number1)==true && Number.isInteger(number2)==true && (number1>=0 && number2>=0)) {
+        let sum=0;
+        largest=Math.max(number1,number2);
+        for (let i=0; i<=largest;i++) {
+            sum+=i
 
-};
+        }
+        return sum
+    } else {
+        return 'ERROR'
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
